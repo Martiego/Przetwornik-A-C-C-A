944bafb1b0ffef711621e9715c7faa670d43b181
Kacper Świercz <kacper.swiercz00@gmail.com>
2020-05-30 12:04:52 +0200

    Add recording

--- src/pl/komponentowe/Main.java
+++ src/pl/komponentowe/Main.java
@@ -1,5 +1,8 @@
 package pl.komponentowe;
 
+import javax.sound.sampled.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
@@ -14,6 +17,33 @@ public class Main {
         System.out.println("2. Play recording");
 
         if (1 == in.nextInt()) {
+            AudioFormat audioFormat = new AudioFormat(8000, 8, 1, true, true);
+            DataLine.Info info = new DataLine.Info(TargetDataLine.class, audioFormat);
+
+            Thread thread = new Thread(() -> {
+                TargetDataLine targetDataLine = null;
+                AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+                File audioFile = new File("record.wav");
+
+                try {
+                    targetDataLine = (TargetDataLine) AudioSystem.getLine(info);
+                    targetDataLine.open(audioFormat);
+                    targetDataLine.start();
+                } catch (LineUnavailableException e) {
+                    e.printStackTrace();
+                }
+                while (true) {
+                    try {
+                        assert targetDataLine != null;
+                        AudioSystem.write(new AudioInputStream(targetDataLine), fileType, audioFile);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            });
+
+            thread.start();
 
         } else if (2 == in.nextInt()) {
 
