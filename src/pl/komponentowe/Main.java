debab52afe6d061ef30c4028cd4d6237f013dbc7
Martiego <marti3go@gmail.com>
2020-05-30 11:18:25 +0200

    Add menu

--- src/pl/komponentowe/Main.java
+++ src/pl/komponentowe/Main.java
@@ -1,8 +1,22 @@
 package pl.komponentowe;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner in = new Scanner(System.in);
+        System.out.println("Authors: ");
+        System.out.println("Kacper Swiercz 224443");
+        System.out.println("Patryk Kolanek 224333");
+
+        System.out.println("1. Record voice");
+        System.out.println("2. Play recording");
+
+        if (1 == in.nextInt()) {
+
+        } else if (2 == in.nextInt()) {
+
+        }
     }
 }
